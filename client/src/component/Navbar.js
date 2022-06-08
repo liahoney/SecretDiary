4752ddea06cba4f2c6840d395877c2202e8f7205
양윤호 <barmus@yang-yunhoui-MacBookPro.local>
2022-06-08 11:50:37 +0900

    [client] login/signup modal 구현

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -5,98 +5,184 @@ import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import logo from "../../src/image/logo.svg"
+import styled from 'styled-components';
+import Modal from "react-modal";
+import { Grid } from "../element"
+import search from '../image/search.svg';
+import closeButton from '../image/closeButton.svg'
+import Login from '../page/Login';
+import Signup from '../page/Signup';
+
 const Navbar = ({ authenticate, setAuthenticate , handleLogout, isLogin}) => {
- 
   let [width, setWidth] = useState(0);
   let navigate = useNavigate();
   const onCheckEnter = (event) => {
     if (event.key === "Enter") {
       navigate(`?q=${event.target.value}`);
     }
+  }
+  /*const [isLogin, setIsLogin] = useState(false)*/ 
+  const [isLoginMode, setIsLoginMode] = useState(true)
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const openModal = () => {
+    setModalIsOpen(true);
   };
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+  const onClickModal = () => {
+    setIsLoginMode(!isLoginMode);
+  };
+
   return (
-    <div>
-      <div className="side-menu" style={{ width: width }}>
-        <button className="closebtn" onClick={() => setWidth(0)}>
-          &times;
-        </button>
-        <div className="side-menu-list" id="menu-list">
-          
-            <li>
-          <button ><a href="/">로그인</a></button>     
-         <button> <a href="/diary">일기쓰기</a> </button>
-         <button><a href="/mydiary">나의일기</a> </button>
-          <button><a href="/friends">친구목록</a></button>
-          <button><a href="/mypage">마이페이지</a></button>
-          <button><a href="/logout">로그아웃</a></button>
-          </li>
-          
-        </div>
-      </div>
-      <div className="nav-header">
-        <div className="burger-menu hide">
-          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
-        </div>
-        <span>
-    {
-        isLogin === false ? <nav>
-            
-            <a href="/signup">회원가입</a>
-            <a href="/">로그인</a>
-            
-        </nav> : 
-        <nav>
-            <a href="/diary">다이어리</a>
-            <a href="/friends">친구목록</a>
-            <a href="/mypage">마이페이지</a>
-            <a href="/main">메인페이지</a>
-            <a href="/mydiary">마이다이어리</a>
-            
-            <button onClick={handleLogout}>Logout</button>
-            
-        </nav>
-        }
-    </span>
-        {isLogin ? (
-          <div onClick={() => setAuthenticate(false)}>
-            <FontAwesomeIcon icon={faUser} />
-            <span>로그아웃</span>
-          </div>
-        ) : (
-          <div onClick={() => navigate("/main")}>
-            <FontAwesomeIcon icon={faUser} />
-            <span>로그인이다</span>
-          </div>
-        )}
-      </div>
+    <React.Fragment>
+      <HeaderContainer>
+        <Grid is_flex padding="16px">
+          <Grid>
+              <img
+                width="100px"
+                margin="100px"
+                src={logo}
+                onClick={() => {
+                  navigate("/");
+                }}
+              />
+          </Grid>    
+          {isLogin ? (
+            <Grid is_flex width="auto" margin="16px">
+              <SearchContainer to="/search">
+                <img width="18px" src={search} />
+              </SearchContainer>
+              <WriteButton
+                onClick={() => {
+                  setAuthenticate(false)
+                }}
+              >
+                로그아웃
+              </WriteButton>
+            </Grid>
+          ) : (
+            <Grid is_flex width="auto" margin="16px">
+              <SearchContainer to="/search">
+                <img width="18px" src={search} />
+              </SearchContainer>
 
-      <div className="nav-logo">
-        <Link to="/diary">
-          <img src={logo}
-            width={100}
-            
-          />
-        </Link>
-      </div>
-      <div class="nav-menu-area">
-        {/* <ul className="menu">
-          <li>
-          <a href="/">로그인</a>
-          <a href="/diary">일기쓰기</a>
-          <a href="/mydiary">나의일기</a>
-          <a href="/friends">친구목록</a>
-          <a href="/mypage">마이페이지</a>
-          <a href="/logout">로그아웃</a>
-          </li>
-        </ul> */}
-
-        {/* <div className="search-box"> */}
-          {/* <FontAwesomeIcon icon={faSearch} /> */}
-          {/* <input type="text" placeholder="친구검색" onKeyPress={onCheckEnter} /> */}
-        {/* </div> */}
-      </div>
-    </div>
+              <LoginButton onClick={() => setModalIsOpen(true)}>
+                로그인
+              </LoginButton>
+              <Modal isOpen={modalIsOpen} close={closeModal} style={modalStyle}>
+                {isLoginMode ? (
+                  <Login onClickModal={onClickModal} />
+                ) : (
+                  <Signup onClickModal={onClickModal} />
+                )}
+                <CloseButton
+                  src={closeButton}
+                  onClick={closeModal}
+                />
+              </Modal>
+            </Grid>
+          )}
+        </Grid>
+      </HeaderContainer>
+    </React.Fragment>
   );
 };
+
+const modalStyle = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(247, 247, 247, 0.8)",
+    transition: "opacity 2000ms ease-in-out",
+  },
+  content: {
+    width: "650px",
+    height: "510px",
+    margin: "auto",
+    border: "none",
+    boxShadow: "0 2px 12px 0 rgba(0, 0, 0, 0.1)",
+  },
+};
+
+const CloseButton = styled.img`
+  width: 11px;
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const HeaderContainer = styled.div`
+  ${(prop) => prop.theme.responsiveContainer};
+  width: 100%;
+  max-width: 1444px;
+  margin: auto;
+  height: 48px;
+  padding: 16px;
+  display: flex;
+  box-sizing: border-box;
+`;
+
+const WriteButton = styled.button`
+  width: 90px;
+  height: 33px;
+  border-radius: 33px;
+  margin: 0px 12px 0px 0px;
+  padding: 7px;
+  box-sizing: border-box;
+  font-size: 16px;
+  font-weight: bold;
+  color: ${(props) => props.theme.main_black};
+  background-color: #ffffff;
+  border: 1.5px solid;
+  border-color: ${(props) => props.theme.main_black};
+  outline: none;
+  &:hover {
+    background-color: ${(props) => props.theme.main_black};
+    color: ${(props) => props.theme.main_white};
+    cursor: pointer;
+  }
+`;
+
+const LoginButton = styled.button`
+  min-width: 80px;
+  height: 33px;
+  border-radius: 33px;
+  margin: 5px;
+  padding: 7px;
+  box-sizing: border-box;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${(props) => props.theme.main_white};
+  background-color: ${(props) => props.theme.main_black};
+  border: 1.5px solid;
+  border-color: ${(props) => props.theme.main_black};
+  outline: none;
+  &:hover {
+    opacity: 0.3;
+    cursor: pointer;
+  }
+`;
+
+const SearchContainer = styled.div`
+  align-self: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5px;
+  background-color: inherit;
+  width: 37px;
+  height: 37px;
+  border-radius: 10rem;
+  &:hover {
+    background-color: rgb(240, 240, 240);
+  }
+`;
 
 export default Navbar;
